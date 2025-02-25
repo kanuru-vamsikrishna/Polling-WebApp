16a55542c4579a97aa67a1f81aa001ed336a00a8
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-02-25 21:01:49 +0530

    adding images for login form

--- Frontend/polling-webapp/src/components/Layout/AuthLayout.jsx
+++ Frontend/polling-webapp/src/components/Layout/AuthLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import polling_display from "../../assets/images/polling_display.png";
 
 const AuthLayout = ({ children }) => {
   return (
@@ -6,6 +7,9 @@ const AuthLayout = ({ children }) => {
       <div className="w-screen h-screen md:w-1/2 px-12 pt-8 pb-12">
         <h2 className="text-lg font-medium text-black">Polling App</h2>
        {children}
+      </div>
+      <div className="hidden md:block w-1/2 h-screen bg-sky-100">
+        <img src={polling_display} alt={polling_display} />
       </div>
     </div>
   )
