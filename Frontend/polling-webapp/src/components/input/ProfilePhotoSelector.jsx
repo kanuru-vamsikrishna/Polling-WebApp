005fba16ba44b2a5209f06ba7dbe277da05f92ef
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-03-03 23:29:29 +0530

    add the flows for choosing, uploading and deleting profile photo

--- Frontend/polling-webapp/src/components/input/ProfilePhotoSelector.jsx
+++ Frontend/polling-webapp/src/components/input/ProfilePhotoSelector.jsx
@@ -5,16 +5,25 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
   const inputRef = useRef()
   const [previewUrl, setPreviewUrl] = useState("");
 
-  const handleImageChange = () => {
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // Update the image state
+      setImage(file)
 
-  }
+      // Generate preview URL from the file
+      const preview = URL.createObjectURL(file)
+      setPreviewUrl(preview);
+    }
+  } 
 
   const handleRemoveImage = () => {
-
+    setImage(null)
+    setPreviewUrl(null);
   }
 
   const onChooseFile = () => {
-
+    inputRef.current.click();
   }
 
   return (
@@ -24,14 +33,26 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
         accept="image/*"
         ref={inputRef}
         onChange={handleImageChange}
-        // className="hidden"
       />
-      <div className="w-20 h-20 flex items-center justify-center bg-sky-100 rounded-full relative">
-      <i className="bi bi-person text-4xl text-blue-400"></i>
-      <button type="button" className="w-8 h-8 items-center justify-center bg-blue-600 text-white rounded-full absolute -bottom-1 -right-1" onClick={onChooseFile}>
-      <i className="bi bi-upload"></i>
-      </button>
+      {!image ? (
+       <div className="w-20 h-20 flex items-center justify-center bg-sky-100 rounded-full relative">
+       <i className="bi bi-person text-4xl text-blue-400"></i>
+       <button type="button" className="w-8 h-8 items-center justify-center bg-blue-600 text-white rounded-full absolute -bottom-1 -right-1" onClick={onChooseFile}>
+       <i className="bi bi-upload"></i>
+         </button>
+       </div>
+      ): (
+        <div className="relative">
+        <img
+          src={previewUrl}
+          alt="Profile Photo"
+          className="w-20 h-20 rounded-full object-cover"
+        />
+        <button type="button" className="w-8 h-8 items-center justify-center bg-blue-600 text-white rounded-full absolute -bottom-1 -right-1" onClick={handleRemoveImage}>
+        <i className="bi bi-trash"></i>
+        </button>
       </div>
+      )}
     </div>
   )
 }
--- Frontend/polling-webapp/src/pages/Auth/SignUpForm.jsx
+++ Frontend/polling-webapp/src/pages/Auth/SignUpForm.jsx
@@ -10,13 +10,13 @@ const SignUpForm = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState(null)
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
   }
 
 
