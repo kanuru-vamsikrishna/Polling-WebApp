990b9e0792d67885c4728131c7e90abf2f7fba86
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-03-20 16:53:21 +0530

    Adding the logic for user registration

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+
+// Generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+}
+
+// Register a new user
+exports.registerUser = async (req, res) => {
+  const { fullName, userName, email, password, profileImageUrl } = req.body;
+
+  // Check for missing fields
+  if (!fullName || !userName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required!" })
+  }
+
+  // User name validation check
+  // Allows alphanumeric characters and hyphens only
+  const userNameRegex = /^[a-zA-Z0-90-]+$/
+  if (!userNameRegex.test(userName)) {
+    return res.status(400).json({ message: "Invalid Username. Only alphanumeric characters and hyphens are allowed" })
+  }
+
+  try {
+    // Check if email already exists
+    const existingUserEmail = await User.findOne({ email })
+    if (existingUserEmail) {
+      return res.status(400).json({ message: "Email already in use" })
+    }
+    // Check if Username exists
+    const existingUserName = await User.findOne({ userName })
+    if (existingUserName) {
+      return res.status(400).json({ message: "User name not available. Try another one" })
+    }
+
+    // Create the User
+    const user = await User.create({
+      fullName,
+      userName,
+      email,
+      password,
+      profileImageUrl
+    });
+
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id)
+    })
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user", error: error.message })
+  }
+
+}
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const connectDB = require("./config/db")
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(
 
 app.use(express.json())
 connectDB()
+
+app.use("/api/v1/auth", authRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
