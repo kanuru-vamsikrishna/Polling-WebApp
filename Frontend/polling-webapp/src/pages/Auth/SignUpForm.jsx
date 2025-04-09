59757b8e0dd932318f0140205c49bfc91641b6b7
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-04-09 21:18:39 +0530

    Add upload apis and integrate it in FE

--- Frontend/polling-webapp/src/pages/Auth/SignUpForm.jsx
+++ Frontend/polling-webapp/src/pages/Auth/SignUpForm.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../components/Layout/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom';
 import AuthInput from '../../components/input/AuthInput';
 import ProfilePhotoSelector from '../../components/input/ProfilePhotoSelector';
 import { validateEmail } from '../../utils/helper';
+import { UserContext } from '../../context/UserContext';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUpForm = () => {
 
@@ -14,6 +18,7 @@ const SignUpForm = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const { updateUser } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -35,7 +40,36 @@ const SignUpForm = () => {
           setError("Please enter the password")
           return;
     }
-        setError("")
+    setError("")
+    
+    // Signup API.
+    try {
+      // Upload image if present
+      let profileImageUrl
+      if (profilePic) {
+        const imageUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imageUploadRes.imageUrl || ""
+      }
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        userName,
+        password,
+        profileImageUrl,
+      })
+      const { token, user } = response.data;
+      if (token) {
+        localStorage.setItem("token", token)
+        updateUser(user)
+        navigate('/dashboard')
+      }
+    } catch (error) {
+     if (error.response && error.response.data.message) {
+        setError(error.response.data.message)
+      } else {
+        setError("Something went wrong. Please try again")
+      }
+    }
   }
 
 
